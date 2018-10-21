68e26130d8763e8947d291bd2e7e71437edcc753
Sandro <sbadame@gmail.com>
2018-10-21 01:33:13 +0000

    Single therm working.

--- print_temperature.py
+++ print_temperature.py
@@ -1,36 +1,44 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
 import time
 
-_USAGE='''./print_temperature $(./list_therms)'''
 
-to_fahrenheit = lambda c: (c * 9.0/5) + 32
+def parse_therm(content, seconds_since_epoch):
+  str_time = '%.0f' % seconds_since_epoch
 
-def read_therm(therm):
-  try:
-    content = open(therm, 'r').read()
-  except KeyboardInterrupt:
-    sys.exit(0)
   lines = content.splitlines()
-  status = 'YES' in lines[0]
+
+  if 'YES' not in lines[0]:
+    return ','.join([str_time, 'ERROR', 0, 0])
+
   _, temp = lines[1].split('t=')
   celsius = int(temp)/1000.0
-  return status, celsius, to_fahrenheit(celsius)
+  to_f = lambda c: (c * 9.0/5) + 32
+  return ','.join([str_time, 'OK', '%2.3f' % celsius, '%2.3f' % to_f(celsius)])
 
 
-def main(thermostat):
+def main(thermostat_file, poll_every_n_seconds):
   while True:
-    temp = read_therm(thermostat)
-    print temp
     try:
-      time.sleep(1)
+      content = open(thermostat_file, 'r').read()
+    except KeyboardInterrupt:
+      sys.exit(0)
+    print(parse_therm(content, time.time()))
+    try:
+      time.sleep(poll_every_n_seconds)
     except KeyboardInterrupt:
       sys.exit(0)
 
+
 if __name__ == "__main__":
   if len(sys.argv) < 2:
-    print _USAGE
+    print('./print_temperature $(./list_therms)')
     sys.exit(1)
   file_name = sys.argv[1]
-  main(file_name)
+  config = {
+    'thermostat_file': file_name,
+    'poll_every_n_seconds': 30,
+  }
+  print(config)
+  main(**config)
