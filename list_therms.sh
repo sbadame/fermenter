220e80318b75893b0674802d014ab5d403b6278c
Sandro <sbadame@gmail.com>
2018-10-21 01:51:33 +0000

    Added modules to be autoloaded on boot.

--- list_therms.sh
+++ list_therms.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo modprobe w1-gpio
-sudo modprobe w1-therm
+# These should be loaded on bootup. Checkout /etc/modules-load.d/modules.conf
+# sudo modprobe w1-gpio
+# sudo modprobe w1-therm
 
 # If you have more than one Sensor connected, you'll see multiple 28-xxx files.
 # Each one will have the unique serial number so you may want to plug one in at
