439d638808c44fe6c50b7239b97203c40f575676
Sandro <sbadame@gmail.com>
2018-11-05 22:40:49 -0500

    Parse the last logged time and print it as part of the health check

--- health_check.sh
+++ health_check.sh
@@ -4,3 +4,4 @@ systemctl --user status fermenter-logging
 grep "" /dev/null data/config.json
 ./list_therms.sh --all
 tail ~/logs/upload.sh.log
+echo "Latest time in data/log.csv: $(date -d @$(tail -n1 data/log.csv | cut -d',' -f1) '+%F %X')" 
