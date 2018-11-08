ec2957e04a3f54fc05d9c54759bae530c57a8402
Sandro <sbadame@gmail.com>
2018-11-07 22:06:41 -0500

    Just use jq for monitoring

--- health_check.sh
+++ health_check.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 systemctl --user status fermenter-logging
-grep "" /dev/null data/config.json
+cat data/config.json | jq
 ./list_therms.sh --all
 tail ~/logs/upload.sh.log
 echo "Latest time in data/log.csv: $(date -d @$(tail -n1 data/log.csv | cut -d',' -f1) '+%F %X')" 
