1d2b8dc21acf5e08f9101703fdbe8a21283c426c
Sandro <sbadame@gmail.com>
2018-11-25 20:34:29 -0500

    Added test cases to the controller

--- controller/controller.py
+++ controller/controller.py
@@ -12,13 +12,23 @@ _UNINITIALIZED = 'uninitialized'
 _COOLING = 'cooling'
 _OFF = 'off'
 
-_PIN = 'GPIO. 0'
+class Gpio(object):
+  _PIN = 'GPIO. 0'
 
-_gpio = lambda args: subprocess.call(['/usr/bin/gpio'] + args)
-_INIT = lambda: _gpio(['mode', _PIN, 'out'])
-_HIGH = lambda: _gpio(['write', _PIN, '1'])
-_LOW = lambda: _gpio(['write', _PIN, '0'])
+  def __init__(self, gpio_path):
+    self._gpio_path = gpio_path
 
+  def _gpio(self, args):
+    subprocess.check_call([self._gpio_path] + args)
+
+  def init(self):
+    self._gpio(['mode', self._PIN, 'out'])
+
+  def high(self):
+    self._gpio(['write', self._PIN, '1'])
+
+  def low(self):
+    self._gpio(['write', self._PIN, '0'])
 
 def read_with_lock(file_path):
   with open(file_path, 'r') as fd:
@@ -43,51 +53,67 @@ def read_state(file_path):
   except FileNotFoundError:
       return State(_UNINITIALIZED)
 
-def write(file_path, state):
-  with open(file_path, 'w') as f:
-    f.write(json.dumps(state._asdict()))
+class Controller(object):
 
-def _cool(state_file):
-  _HIGH()
-  write(state_file, State(_COOLING))
+  def __init__(self, gpio, state_file):
+    self._gpio = gpio
+    self._state_file = state_file
 
-def _off(state_file):
-  _LOW()
-  write(state_file, State(_OFF))
+  def _write(self, state):
+    with open(self._state_file, 'w') as f:
+      f.write(json.dumps(state._asdict()))
 
-def decide(realtime_log, monitor_thermometer, controller_state, desired, threshold):
+  def cool(self):
+    self._gpio.high()
+    self._write(State(_COOLING))
+
+  def off(self):
+    self._gpio.low()
+    self._write(State(_OFF))
+
+
+def decide(gpio, realtime_log, monitor_thermometer, controller_state, desired, threshold):
+  controller = Controller(gpio, controller_state)
   temp = read_temp(realtime_log, monitor_thermometer)
+
   s = read_state(controller_state)
 
   if s.state == _UNINITIALIZED:
     if temp < desired:
-      _off(controller_state)
+      controller.off()
     else:
-      _cool(controller_state)
+      controller.cool()
   elif s.state == _OFF:
     if temp > desired + threshold:
-      _cool(controller_state)
+      controller.cool()
   elif s.state == _COOLING:
     if temp < desired - threshold:
-      _off(controller_state)
+      controller.off()
   else:
     raise Exception('Unhandled state: %s' % s)
 
 def _main(poll_seconds, *args):
-  while True:
+  if poll_seconds < 0:
     decide(*args)
-    time.sleep(poll_seconds)
+    sys.exit(0)
+  else:
+    while True:
+      decide(*args)
+      time.sleep(poll_seconds)
 
 if __name__ == "__main__":
   with open(sys.argv[1], 'r') as fd:
     config = json.loads(fd.read())
+
+  gpio = Gpio(config['gpio_path'])
   args = [
     config['control_every_n_seconds'],
+    gpio,
     config['realtime_log'],
     config['monitor_thermometer'],
     config['controller_state'],
     config['desired_temp_celsius'],
     config['threshold_degrees_celsius'],
   ]
-  _INIT()
+  gpio.init()
   _main(*args)
