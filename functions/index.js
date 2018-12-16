f675bc577d7659fd1ba937eb063b1587a8f50964
Sandro <sbadame@gmail.com>
2018-12-15 23:57:32 -0500

    All ui data is now a single json fetch away.

--- functions/index.js
+++ functions/index.js
@@ -12,9 +12,7 @@ const THERMS = {
 const functions = require('firebase-functions');
 const {Storage} = require('@google-cloud/storage');
 const storage = new Storage();
-const bucket = functions.config().gcs.bucket;
-const fileName = functions.config().gcs.filename;
-const gcsFile = storage.bucket(bucket).file(fileName);
+const gcsFile = storage.bucket(JSON.parse(process.env.FIREBASE_CONFIG).storageBucket).file('log.json');
 
 // Initialize the Firebase application with admin credentials
 const admin = require('firebase-admin');
@@ -40,7 +38,7 @@ function paginate(base_query, last_seen, collector) {
   });
 }
 
-exports.updateCache = functions.runWith(runtimeOpts).firestore.document('ui/upload').onWrite(() => {
+function queryTempLogEntries() {
   const query = fs.collection("logentries").orderBy('timestamp').limit(PAGE_SIZE);
   return paginate(query).then(results => {
     var fireLogs = [];
@@ -49,8 +47,25 @@ exports.updateCache = functions.runWith(runtimeOpts).firestore.document('ui/uplo
       var f = (data.temperature_celsius * (9.0/5.0)) + 32;
       fireLogs.push([data.timestamp.seconds, THERMS[data.thermometer_name], data.status, f]);
     });
+    return fireLogs;
+  });
+}
+
+function queryControllerLogEntries() {
+  return fs.collection('controller_logentries').orderBy('timestamp').get().then(results => {
+    const entries = results.docs.map(d => d.data());
+    for (let entry of entries) {
+      entry['timestamp'] = entry['timestamp']['_seconds'];
+    }
+    return entries;
+  });
+}
+
+exports.updateCache = functions.runWith(runtimeOpts).firestore.document('ui/upload').onWrite(() => {
+  return Promise.all([queryTempLogEntries(), queryControllerLogEntries()]).then(results => {
+    const json = JSON.stringify({'temp_logs': results[0], 'controller_state': results[1]});
     const writer = gcsFile.createWriteStream({resumable: false});
-    writer.write(JSON.stringify(fireLogs));
+    writer.write(json);
     writer.end();
     return 0;
   });
